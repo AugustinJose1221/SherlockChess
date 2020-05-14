240eacc8db4f84ad752087a5c7754d827f941286
AugustinJose1221 <augustinjose1221@gmail.com>
2020-05-14 14:29:16 +0530

    Minor changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import flask
 from stockfish import Stockfish
 s = Stockfish("bin/stockfish")
 s.set_position(["e2e4", "e7e6"])
 #stockfish.get_board_visual()
 #s.get_board_visual()
 print(s.get_best_move())
-print(s)
